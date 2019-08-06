a345ffa9eec76620ae9c913a420a2aaccbff8252
Sidnei <xplouses@gmail.com>
2019-08-06 07:36:45 -0300

    project created

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
