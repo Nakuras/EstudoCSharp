42a7c0256996ef75c819ed63f541d6db5d0dd658
Sidnei <xplouses@gmail.com>
2019-08-10 16:19:39 -0300

    upgrade in inprint board

--- Program.cs
+++ Program.cs
@@ -12,9 +12,13 @@ namespace ChessUdemy
     {
         static void Main(string[] args)
         {
-            ChessPosition chessPosition = new ChessPosition('c', 7);
+            Board board = new Board(8, 8);
 
-            Console.WriteLine(chessPosition);
+            board.PutPiece(new Tower(board, Color.Black), new Position(0, 0));
+            board.PutPiece(new Tower(board, Color.Black), new Position(1, 3));
+            board.PutPiece(new King(board, Color.Black), new Position(0, 2));
+
+            Screen.PrintBoard(board);
 
             Console.ReadLine();
         }
--- Screen.cs
+++ Screen.cs
@@ -13,6 +13,7 @@ namespace ChessUdemy
         {
             for (int i = 0; i < board.lines; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.colums; j++)
                 {
                     if (board.Piece(i, j) == null)
@@ -21,11 +22,27 @@ namespace ChessUdemy
                     }
                     else
                     {
-                        Console.Write(board.Piece(i, j) + " ");
+                        ImprintPiece(board.Piece(i, j));
+                        Console.Write(" ");
                     }
                 }
-
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void ImprintPiece(Piece piece)
+        {
+            if (piece.color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor auxiliarColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = auxiliarColor;
             }
         }
     }
